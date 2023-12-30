e1e059ef6d41959b4d3e60469cc0741adcaa3ee3
zhiyuan <1258586900@qq.com>
2023-12-31 07:45:32 +0800

    提交预处理工具代码

---
+++ ProcessingProgram/team/team.js
@@ -0,0 +1,34 @@
+
+// 导入 fs 和 path 模块
+import * as fs from 'fs';
+import * as path from "path";
+// 导入 fileURLToPath 方法
+import { fileURLToPath } from 'url'
+// 获取当前文件路径
+const __filenameNew = fileURLToPath(import.meta.url)
+// 获取当前文件所在的目录
+const __dirnameNew = path.dirname(__filenameNew)
+// 获取 teamDataPath 路径
+let teamDataPath = path.resolve(`${__dirnameNew}`,"./team/data/");
+
+
+// 读取 teamDataPath 目录下的文件
+let items =  fs.readdirSync(teamDataPath)
+// 初始化 dataAllText
+let dataAllText = "[";
+// 遍历 items 数组
+for(let index in items ){
+    // 读取每一个文件的内容
+    let itemtext = fs.readFileSync(path.resolve(`${teamDataPath}`,items[index]),"utf-8")
+    // 打印文件内容
+    console.log(itemtext);
+    // 将文件内容拼接到 dataAllText 中
+    dataAllText = dataAllText + itemtext + ",";
+}
+// 将 dataAllText 末尾的逗号去掉
+dataAllText = dataAllText.substring(0,dataAllText.length-1);
+// 将 dataAllText 末尾加上括号
+dataAllText = dataAllText + "]";
+// 将 dataAllText 写入到当前文件所在的目录下的 dataAll.json 文件中
+fs.writeFileSync(path.resolve(`${__dirnameNew}`,`./team/dataAll.json`),dataAllText);
+
